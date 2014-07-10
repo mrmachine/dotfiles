aad17977bd5024f87a88d0ee4dc4ec4b51cc5cc8
Tai Lee <real.human@mrmachine.net>
2014-07-11 00:29:04 +1000

    Backup existing SSH config before overwriting.

--- home/.zsh/rc.d/ssh-config.zsh
+++ home/.zsh/rc.d/ssh-config.zsh
@@ -1,7 +1,10 @@
 # Combine SSH configs.
 if [ -d ~/.ssh/config.d ]; then
-	rm -f ~/.ssh/config
-	echo "# This file has been automatically generated. Do not edit it directly." > ~/.ssh/config
+	HEAD="# This file has been automatically generated."
+	if [[ -f ~/.ssh/config && `head -n 1 ~/.ssh/config` != $HEAD ]]; then
+		mv ~/.ssh/config ~/.ssh/config-`date +%Y%m%d`
+	fi
+	echo $HEAD >! ~/.ssh/config
 	for filename in ~/.ssh/config.d/*; do
 		echo "\n#\n# $filename\n#\n" >> ~/.ssh/config
 		cat $filename >> ~/.ssh/config
