d40329a8c1c0617d847812dfb449d740aa19c970
Tai Lee <real.human@mrmachine.net>
2014-07-16 00:40:09 +1000

    Use Sublime Text as the default visual editor, too.

--- home/.zsh/rc.d/tailee.zsh
+++ home/.zsh/rc.d/tailee.zsh
@@ -24,6 +24,7 @@ alias sub='"/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"'
 
 # Use Sublime Text as the default editor.
 export EDITOR="subl -n -w"
+export VISUAL=$EDITOR
 
 # Add ~/bin to `PATH`.
 export PATH=$HOME/bin:$PATH
