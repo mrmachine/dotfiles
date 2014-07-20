94621ec6ef497c19551c57414ebc472dd6c3d4f2
Tai Lee <real.human@mrmachine.net>
2014-07-20 21:34:39 +1000

    Make bootstrap script idempotent.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,35 +3,31 @@ hash zsh 2>/dev/null || {
 	exit 1
 }
 
-if [ -d ~/.homesick ]; then
-	echo >&2 "~/.homesick directory already exists. Aborting."
-	exit 1
+ZSH="$(which zsh)"
+if [ "$ZSH" != "$SHELL" ]; then
+	echo "Changing shell to Zsh."
+	if [ -z "$(grep $ZSH /etc/shells)" ]; then
+		echo "$ZSH" | sudo tee -a /etc/shells
+	fi
+	chsh -s "$ZSH"
 fi
 
-echo "Installing Homeshick."
-git clone https://github.com/andsens/homeshick.git ~/.homesick/repos/homeshick
-source ~/.homesick/repos/homeshick/homeshick.sh
+if [ ! -d "$HOME/.homesick/repos/homeshick" ]; then
+	echo "Installing Homeshick."
+	git clone https://github.com/andsens/homeshick.git "$HOME/.homesick/repos/homeshick"
+fi
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 
 echo "Cloning Homeshick castles."
-homeshick clone -b DamnWidget/anaconda
-homeshick clone -b jisaacks/GitGutter
-homeshick clone -b mrmachine/dotfiles
-homeshick clone -b sindresorhus/editorconfig-sublime
-homeshick clone -b sorin-ionescu/prezto
-homeshick clone -b squ1b3r/Djaneiro
-homeshick clone -b wbond/sublime_package_control
+[ ! -d "$HOME/.homesick/repos/anaconda" ] && homeshick clone -b DamnWidget/anaconda
+[ ! -d "$HOME/.homesick/repos/Djaneiro" ] && homeshick clone -b squ1b3r/Djaneiro
+[ ! -d "$HOME/.homesick/repos/dotfiles" ] && homeshick clone -b mrmachine/dotfiles
+[ ! -d "$HOME/.homesick/repos/editorconfig-sublime" ] && homeshick clone -b sindresorhus/editorconfig-sublime
+[ ! -d "$HOME/.homesick/repos/GitGutter" ] && homeshick clone -b jisaacks/GitGutter
+[ ! -d "$HOME/.homesick/repos/prezto" ] && homeshick clone -b sorin-ionescu/prezto
+[ ! -d "$HOME/.homesick/repos/sublime_package_control" ] && homeshick clone -b wbond/sublime_package_control
 
 echo "Linking Homeshick castles."
 homeshick link
-
-ZSH="$(which zsh)"
-
-if [ $ZSH != $SHELL ]; then
-	echo "Changing shell to Zsh."
-	if [ -z "$(grep $ZSH /etc/shells)" ]; then
-		echo $ZSH | sudo tee -a /etc/shells
-	fi
-	chsh -s $ZSH
-fi
 
 echo "Installed. Open a new terminal window or tab."
