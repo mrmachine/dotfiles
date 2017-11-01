3ea93abf6b07c235b87d55eb341eee7721fee295
Tai Lee <real.human@mrmachine.net>
2017-11-02 10:59:56 +1100

    Add Visual Studio Code to PATH.

--- home/.zsh/zshenv.d/tailee.zsh
+++ home/.zsh/zshenv.d/tailee.zsh
@@ -38,3 +38,6 @@ export PYTHONWARNINGS=ignore
 # Build Python with framework/shared support (for PyInstaller).
 # See: https://github.com/yyuu/pyenv/wiki#how-to-build-cpython-with-framework-support-on-os-x
 export PYTHON_CONFIGURE_OPTS="--enable-framework"
+
+# Visual Studio Code.
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
